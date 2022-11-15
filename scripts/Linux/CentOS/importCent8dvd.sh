228c5cab3b710ea8fbaa106788a2ce3d8febca6b
Derrick Rice <108289028+Tekn0logy@users.noreply.github.com>
2022-11-15 17:43:47 -0500

    Including logic to properly test for new environment No sudo when working in home directory Change perms and ownership to user

--- scripts/Linux/CentOS/importCent8dvd.sh
+++ scripts/Linux/CentOS/importCent8dvd.sh
@@ -1,10 +1,21 @@
 #!/bin/bash
 if ! [ -d ~/working/dvd ]; then
-   sudo mkdir -vp ~/working/dvd
+   mkdir -vp ~/working/dvd
+fi
+
+if [ -d ~/gitLocal/CentOS-8-Stream-DVD/ ]; then
+   sudo rm -rf ~/gitLocal/CentOS-8-Stream-DVD/.??*
+else
+   mkdir -vpm 775 ~/gitLocal/CentOS-8-Stream-DVD/
 fi
 
 sudo mount -vo ro /mnt/hgfs/Download/ISOs/CentOS/CentOS-Stream-8-x86_64-latest-dvd1.iso ~/working/dvd
 shopt -s dotglob
-cp -arv --sparse=always ~/working/dvd/ ~/gitLocal/CentOS-8-Stream-DVD/
+cp -arv --sparse=always ~/working/dvd/* ~/gitLocal/CentOS-8-Stream-DVD/
+cp -arv --sparse=always ~/working/dvd/.??* ~/gitLocal/CentOS-8-Stream-DVD/
 touch ~/gitLocal/CentOS-8-Stream-DVD/.gitignore
 sudo umount -v ~/working/dvd
+
+sudo chmod -vR ug+w ~/gitLocal/CentOS-8-Stream-DVD/isolinux/ ~/gitLocal/CentOS-8-Stream-DVD/EFI/BOOT/
+sudo mkdir -vpm 755 ~/gitLocal/CentOS-8-Stream-DVD/bootConfigs
+sudo chown -v $USER:$USER ~/gitLocal/CentOS-8-Stream-DVD/bootConfigs
--- scripts/Linux/CentOS/makeCent8DVD.sh
+++ scripts/Linux/CentOS/makeCent8DVD.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 publishDate=$(date +%Y%m%d%H%M)
 cd ~/gitLocal/CentOS-8-Stream-DVD/
-cp -av ~/gitLocal/CentOS-Installers/8/isolinux/isolinux.cfg ~/gitLocal/CentOS-8-Stream-DVD/dvd/isolinux/
-cp -av ~/gitLocal/CentOS-Installers/8/EFI/BOOT/* ~/gitLocal/CentOS-8-Stream-DVD/dvd/EFI/BOOT/
-cp -av ~/gitLocal/CentOS-Installers/8/bootConfigs ~/gitLocal/CentOS-8-Stream-DVD/dvd/
+cp -av ~/gitLocal/CentOS-Installers/8/isolinux/isolinux.cfg ~/gitLocal/CentOS-8-Stream-DVD/isolinux/
+cp -av ~/gitLocal/CentOS-Installers/8/EFI/BOOT/* ~/gitLocal/CentOS-8-Stream-DVD/EFI/BOOT/
+cp -av ~/gitLocal/CentOS-Installers/8/bootConfigs/* ~/gitLocal/CentOS-8-Stream-DVD/bootConfigs/
 xorriso -as mkisofs -v -x -J -R -o "/mnt/hgfs/Download/ISOs/CentOS/$publishDate-CentOS-Stream-8-x86_64-Custom-dvd.iso" \
 -V "CentOS8x64-Custom" -publisher "Tekn0logy <https://github.com/Tekn0logy>" \
 -b "isolinux/isolinux.bin" -c "isolinux/boot.cat" -no-emul-boot -boot-load-size 4 -boot-info-table \
--eltorito-alt-boot -e images/efiboot.img -no-emul-boot -isohybrid-gpt-basdat -m TRANS.TBL -graft-points "dvd"
+-eltorito-alt-boot -e 'images/efiboot.img' -no-emul-boot -isohybrid-gpt-basdat -part_like_isohybrid \
+-iso_mbr_part_type 0x00 --mbr-force-bootable -partition_offset 0 -partition_cyl_align off -m TRANS.TBL -graft-points ~/gitLocal/CentOS-8-Stream-DVD/
 # cd ~/
